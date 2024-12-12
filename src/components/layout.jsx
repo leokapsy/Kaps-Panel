dfb33bfcc595f797af784f591b4c17006ec5a2e9
leokapsy <leokapfy@gmail.com>
2024-12-12 06:55:08 +0200

    new committed

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Outlet } from "react-router-dom"
 import Header from './header'
 import Footer from  './footer'
+import NavBar from './NavBar'
 
 
 const layout = () => {
   return (
     <>
-        <Header/>
+        <Header>
+          <NavBar/>
+        </Header>
         
             <Outlet/>
         <Footer/>
