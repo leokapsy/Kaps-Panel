b37c152a17dcf5994e992e6fc9a89618cd09d5ce
leokapsy <111013998+leokapsy@users.noreply.github.com>
2024-12-12 19:56:39 +0200

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { MdMarkEmailRead } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
   return (
     <div className='sidenav'>
-      <h3>Kaps Panel</h3>
+      <h3 className='logo'>Kaps Panel</h3>
       <ul>
         <li><Link to="/dashboard">Dashboard</Link></li>
         <li><Link to="/emails">Emails</Link></li>
         <li><Link to="/terminal">Terminal</Link></li>
         <li><Link to="/domains">Domains</Link></li>
         <li><Link to="/monitoring">Monitoring</Link></li>
+    </ul>
+    <ul className='signin'>
+        <li><Link to="/logout">Logout</Link></li>
+        
     </ul>
 
     </div>
