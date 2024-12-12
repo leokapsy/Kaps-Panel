fa1eebc6dd5d16d5abe9587184be0a709f9bca0d
leokapsy <111013998+leokapsy@users.noreply.github.com>
2024-12-12 19:53:20 +0200

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import { Outlet } from "react-router-dom"
-import Header from './header'
-import Footer from  './footer'
-import NavBar from './NavBar'
+
+
 
 
 const layout = () => {
   return (
     <>
-        <Header>
-          <NavBar/>
-        </Header>
         
             <Outlet/>
-        <Footer/>
+        
     </>
   )
 }
